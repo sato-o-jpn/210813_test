98c9cf74764f346485126bb8d7b91268f163cfc8
Satoshi Okayasu <okaka.sato.o.jpn@gmail.com>
2021-08-13 13:42:12 +0900

    1st commit

---
+++ pybotweb_test.py
@@ -0,0 +1,12 @@
+from bottle import route, run, template, request
+
+@route('/hello')
+def hello():
+    return template('pybot_template', text="")
+
+@route('/hello', method='POST')
+def do_hello():
+    input_text = request.forms.input_text
+    return template('pybot_template', text=input_text)
+
+run(host='localhost', port=8080, debug=True)
